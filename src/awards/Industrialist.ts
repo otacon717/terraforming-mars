264583e4c0395d6555ec86a7b1ce11eeb708c877
flychai <iamflychai@gmail.com>
2020-08-14 13:56:04 +0800

    Awards calculation

--- src/awards/Industrialist.ts
+++ src/awards/Industrialist.ts
@@ -1,11 +1,18 @@
 import { IAward } from "./IAward";
 import { Player } from "../Player";
 import { Game } from "../Game";
+import { Phase } from "../Phase";
+import { Resources } from "../Resources";
 
 export class Industrialist implements IAward {
     public name: string = "Industrialist";
     public description: string = "Having most steel and energy resources"
     public getScore(player: Player, _game: Game): number {
-        return player.steel + player.energy;
+        if (_game.phase === Phase.END){
+		return player.steel + player.energy;	
+	}else
+	{
+		return player.steel + player.getProduction(Resources.STEEL) + player.getProduction(Resources.ENERGY);
+	}
     }   
 }
